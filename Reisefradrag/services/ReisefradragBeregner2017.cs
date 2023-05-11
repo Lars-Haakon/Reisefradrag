92706391c3f95deb347167e87bace9372180a31e
Lars-Håkon Nohr Nystad <lnn@dips.no>
2023-05-11 20:39:13 +0200

    Implementert avstandsberegning av arbeidsreiser og tester til beregninger av arbeidsreiser

--- Reisefradrag/services/ReisefradragBeregner2017.cs
+++ Reisefradrag/services/ReisefradragBeregner2017.cs
@@ -4,9 +4,52 @@ namespace Reisefradrag.services
 {
     public class ReisefradragBeregner2017 : IReisefradragBeregner
     {
+        const decimal EgenAndel = 22000M;
+
         public decimal BeregnFradrag(List<Arbeidsreise> arbeidsReiser, List<Besoeksreise> besoeksreiser, int utgifterBomFergeEtc)
         {
-            return 0.0M;
+            var avstandsUtgifter = GetAvstandsUtgifter(arbeidsReiser, besoeksreiser);
+
+            var fradrag = avstandsUtgifter - EgenAndel;
+            if(fradrag < 0)
+            {
+                return 0;
+            }
+
+            return fradrag;
+        }
+
+        private decimal GetAvstandsUtgifter(List<Arbeidsreise> arbeidsReiser, List<Besoeksreise> besoeksreiser)
+        {
+            var totalAvstand = GetTotalAvstand(arbeidsReiser, besoeksreiser);
+
+            decimal avstandsUtgifter = 0M;
+            if (totalAvstand > 50000)
+            {
+                var avstandOver50K = totalAvstand - 50000;
+                avstandsUtgifter += avstandOver50K * 0.7M;
+                totalAvstand -= avstandOver50K;
+            }
+
+            avstandsUtgifter += totalAvstand * 1.5M;
+
+            return avstandsUtgifter;
+        }
+
+        private long GetTotalAvstand(List<Arbeidsreise> arbeidsReiser, List<Besoeksreise> besoeksreiser)
+        {
+            long totalAvstand = 0;
+            foreach (var arbReise in arbeidsReiser)
+            {
+                totalAvstand += arbReise.antall * arbReise.km;
+            }
+
+            if(totalAvstand > 75000)
+            {
+                return 75000;
+            }
+
+            return totalAvstand;
         }
     }
 }
---
+++ TestReisefradrag/TestReisefradragArbeidsreiser.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reisefradrag.Dto;
+using Reisefradrag.services;
+
+namespace TestReisefradrag
+{
+    [TestClass]
+    public class TestReisefradragArbeidsreiser
+    {
+        public ReisefradragBeregner2017 FradragsBeregner { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            FradragsBeregner = new ReisefradragBeregner2017();
+        }
+
+        [TestMethod]
+        public void IngenArbeidsreiser_ReturnIngenFradrag()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>();
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(0, fradrag);
+        }
+
+        [TestMethod]
+        public void EnArbeidsreise_Under50Kantall1()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 30000,
+                    antall = 1
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(23000, fradrag);
+        }
+
+        [TestMethod]
+        public void EnArbeidsreise_Over50Kantall1()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 60000,
+                    antall = 1
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(60000, fradrag);
+        }
+
+        [TestMethod]
+        public void EnArbeidsreise_Over75Kantall1()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 80000,
+                    antall = 1
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(70500, fradrag);
+        }
+
+        [TestMethod]
+        public void EnArbeidsreise_Over50Kantall2()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 30000,
+                    antall = 2
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(60000, fradrag);
+        }
+
+        [TestMethod]
+        public void ToArbeidsreiser_Under50Kantall1()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 10000,
+                    antall = 1
+                },
+                new Arbeidsreise()
+                {
+                    km = 15000,
+                    antall = 1
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(15500, fradrag);
+        }
+
+        [TestMethod]
+        public void ToArbeidsreiser_Over50Kantall3()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 10000,
+                    antall = 3
+                },
+                new Arbeidsreise()
+                {
+                    km = 15000,
+                    antall = 3
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(70500, fradrag);
+        }
+
+        [TestMethod]
+        public void MangeArbeidsreiser_UliktAntall()
+        {
+            var arbeidsReiser = new List<Arbeidsreise>()
+            {
+                new Arbeidsreise()
+                {
+                    km = 7295,
+                    antall = 2
+                },
+                new Arbeidsreise()
+                {
+                    km = 19245,
+                    antall = 1
+                },
+                new Arbeidsreise()
+                {
+                    km = 1,
+                    antall = 100
+                }
+                ,
+                new Arbeidsreise()
+                {
+                    km = 1239,
+                    antall = 13
+                }
+            };
+
+            var fradrag = FradragsBeregner.BeregnFradrag(arbeidsReiser, new(), 0);
+
+            Assert.AreEqual(53029.4M, fradrag);
+        }
+    }
+}
