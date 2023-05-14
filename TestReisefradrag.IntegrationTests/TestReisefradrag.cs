c5d4feac3c35c7b6f20c4a619dc782b8d99b8aa2
Lars-Håkon Nohr Nystad <lnn@dips.no>
2023-05-14 20:38:33 +0200

    Lagt til integrasjonstest for eksempelet gitt i oppgaveteksten

--- TestReisefradrag.IntegrationTests/TestReisefradrag.cs
+++ TestReisefradrag.IntegrationTests/TestReisefradrag.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reisefradrag.Dto;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
@@ -34,6 +35,31 @@ namespace TestReisefradrag.IntegrationTests
             var responseContent = JsonSerializer.Deserialize<ReisefradragResponse>(await response.Content.ReadAsStringAsync());
 
             Assert.AreEqual(0M, responseContent.reisefradrag);
+        }
+
+        [TestMethod]
+        public async Task ExampleRequest_ReturnsCorrectfradrag()
+        {
+            var request = new ReisefradragRequest
+            {
+                arbeidsreiser = new()
+                {
+                    new Arbeidsreise { km=91, antall=180 },
+                    new Arbeidsreise { km=378, antall=4 },
+                },
+                besoeksreiser = new()
+                {
+                    new Besoeksreise { km=580, antall=4 },
+                },
+                utgifterBomFergeEtc = 4850
+            };
+
+            HttpContent content = new StringContent(JsonSerializer.Serialize(request), new MediaTypeHeaderValue("application/json"));
+
+            var response = await _httpClient.PostAsync("/Reisefradrag", content);
+            var responseContent = JsonSerializer.Deserialize<ReisefradragResponse>(await response.Content.ReadAsStringAsync());
+
+            Assert.AreEqual(13168M, responseContent.reisefradrag);
         }
     }
 }
